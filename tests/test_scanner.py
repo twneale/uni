4331eee4c414bd219f40808165dad45a180664dc
twneale <twneale@gmail.com>
2014-05-14 08:51:58 -0400

    Renamed TestCaseScanner to AssertionSet

--- tests/test_scanner.py
+++ tests/test_scanner.py
@@ -2,7 +2,7 @@ import unittest
 
 from nose.tools import raises
 
-from uni.scanner import TestCaseScanner, TestCaseFailed
+from uni.assertions import AssertionSet, AssertionFailed
 
 
 class TestScannerCheck(unittest.TestCase):
@@ -14,7 +14,7 @@ class TestScannerCheck(unittest.TestCase):
     ]
 
     def test_scanner_check(self):
-        with TestCaseScanner(self.querylist) as scanner:
+        with AssertionSet(self.querylist) as scanner:
             scanner.check({'moocow': True})
             scanner.check({'age': 300})
             scanner.check({'age': 3})
@@ -23,22 +23,22 @@ class TestScannerCheck(unittest.TestCase):
         self.assertTrue(scanner.success)
 
     def test_scanner_check_false(self):
-        with TestCaseScanner(self.querylist) as scanner:
+        with AssertionSet(self.querylist) as scanner:
             scanner.check({'moocow': True})
             scanner.check({'age': 300})
             scanner.check({'cowtype': 'wee'})
         self.assertFalse(scanner.success)
 
-    @raises(TestCaseFailed)
+    @raises(AssertionFailed)
     def test_scanner_check_raises(self):
-        with TestCaseScanner(self.querylist, raise_failure=True) as scanner:
+        with AssertionSet(self.querylist, raise_failure=True) as scanner:
             scanner.check({'moocow': True})
             scanner.check({'age': 300})
             scanner.check({'cowtype': 'wee'})
         self.assertTrue(scanner.success)
 
     def test_scanner_check_all(self):
-        scanner = TestCaseScanner(self.querylist)
+        scanner = AssertionSet(self.querylist)
         datalist = [
             {'moocow': True},
             {'age': 300},
@@ -48,16 +48,16 @@ class TestScannerCheck(unittest.TestCase):
         self.assertTrue(scanner.check_all(datalist))
 
     def test_scanner_check_all_false(self):
-        scanner = TestCaseScanner(self.querylist)
+        scanner = AssertionSet(self.querylist)
         datalist = [
             {'moocow': True},
             {'age': 300},
             {'cowtype': 'wee'}]
         self.assertFalse(scanner.check_all(datalist))
 
-    @raises(TestCaseFailed)
+    @raises(AssertionFailed)
     def test_scanner_check_all_raises(self):
-        scanner = TestCaseScanner(self.querylist, raise_failure=True)
+        scanner = AssertionSet(self.querylist, raise_failure=True)
         datalist = [
             {'moocow': True},
             {'age': 300},
