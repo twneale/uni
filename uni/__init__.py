32d49c30cde979501bc182b3bb48fe90bcb26d57
twneale <twneale@gmail.com>
2014-05-15 13:55:35 -0400

    Import things at top level

--- uni/__init__.py
+++ uni/__init__.py
@@ -1,1 +1,7 @@
-from uni.checker import check
+from uni.checker import (
+    check, InvalidQuery, InvalidPath, UnhandledOperatorError)
+
+from uni.assertions import (
+    MatchSet, MatchSetFailed,
+    AssertionSet, AssertionFailed
+    )
